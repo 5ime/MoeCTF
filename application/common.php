01af52bb4585bef539d979629df1d5cd1f4bc913
I Am I <31686695+5ime@users.noreply.github.com>
2022-12-17 20:38:42 +0800

    add: 新增用户真实IP获取

--- application/common.php
+++ application/common.php
@@ -66,3 +66,20 @@ function sendEmail($tomail, $name, $randCode, $attachment = null) {
     }
     return $mail->Send() ? returnJsonData(200,'success') : returnJsonData(201,'error'); //$mail->ErrorInfo;
 }
+
+function getUserIP(){
+    $ip = false;
+    if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+        $ip = $_SERVER["HTTP_CLIENT_IP"];
+    }
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+        if ($ip) { array_unshift($ips, $ip); $ip = FALSE; }
+        for ($i = 0; $i < count($ips); $i++) {
+            if (preg_match("/^(10|172\.16|192\.168)\./i", $ips[$i])) {
+                continue;
+            }
+        }
+    }
+    return ($ip ? $ip : $_SERVER['REMOTE_ADDR']); 
+}
