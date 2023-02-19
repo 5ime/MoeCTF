6cd0f726720ec9630061e1df795f16c803f04181
I Am I <31686695+5ime@users.noreply.github.com>
2023-02-19 17:52:42 +0800

    Update

--- moectf.sql
+++ moectf.sql
@@ -1,141 +1,159 @@
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `buys`
+-- Table structure for buys
 -- ----------------------------
 DROP TABLE IF EXISTS `buys`;
-CREATE TABLE `buys` (
+CREATE TABLE `buys`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL COMMENT '用户id',
   `cid` int(11) NOT NULL COMMENT '挑战id',
-  `time` varchar(255) DEFAULT NULL COMMENT '购买时间',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '购买时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `categorys`
+-- Table structure for categorys
 -- ----------------------------
 DROP TABLE IF EXISTS `categorys`;
-CREATE TABLE `categorys` (
+CREATE TABLE `categorys`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL COMMENT '分类标题',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '分类标题',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `challenges`
+-- Table structure for challenges
 -- ----------------------------
 DROP TABLE IF EXISTS `challenges`;
-CREATE TABLE `challenges` (
+CREATE TABLE `challenges`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL COMMENT '挑战标题',
-  `state` int(1) NOT NULL DEFAULT '0' COMMENT '挑战标题状态 0 隐藏 1 显示',
-  `content` longtext COMMENT '挑战描述',
-  `hint` varchar(255) DEFAULT NULL COMMENT '挑战提示',
-  `flag` varchar(255) NOT NULL COMMENT '挑战答案',
-  `download` text COMMENT '挑战附件地址',
-  `money` varchar(255) NOT NULL COMMENT '挑战需要的金币',
-  `score` varchar(255) NOT NULL DEFAULT '0' COMMENT '挑战分数',
-  `solve` varchar(255) NOT NULL DEFAULT '0' COMMENT '解题成功人数',
-  `tags` text COMMENT '挑战标签',
-  `category` varchar(255) NOT NULL COMMENT '挑战分类',
-  `time` varchar(255) NOT NULL COMMENT '挑战添加时间',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '挑战标题',
+  `state` int(1) NOT NULL DEFAULT 0 COMMENT '挑战标题状态 0 隐藏 1 显示',
+  `content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '挑战描述',
+  `hint` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '挑战提示',
+  `flag` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '挑战答案',
+  `download` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '挑战附件地址',
+  `money` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '挑战需要的金币',
+  `score` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '挑战分数',
+  `solve` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '解题成功人数',
+  `tags` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '挑战标签',
+  `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '挑战分类',
+  `time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '挑战添加时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `money`
+-- Table structure for money
 -- ----------------------------
 DROP TABLE IF EXISTS `money`;
-CREATE TABLE `money` (
+CREATE TABLE `money`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL COMMENT '用户id',
   `money` int(11) NOT NULL COMMENT '金币数量',
-  `type` int(11) NOT NULL DEFAULT '0' COMMENT '增减类型 0 买附件减 1 解题加 2 签到加 3 系统奖励',
-  `time` varchar(255) NOT NULL COMMENT '金币增减时间',
+  `type` int(11) NOT NULL DEFAULT 0 COMMENT '增减类型 0 买附件减 1 解题加 2 签到加 3 系统奖励',
+  `time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '金币增减时间',
   `first` int(11) NOT NULL,
   `last` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `notify`
+-- Table structure for notify
 -- ----------------------------
 DROP TABLE IF EXISTS `notify`;
-CREATE TABLE `notify` (
+CREATE TABLE `notify`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
-  `time` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `setting`
+-- Table structure for setting
 -- ----------------------------
 DROP TABLE IF EXISTS `setting`;
-CREATE TABLE `setting` (
+CREATE TABLE `setting`  (
   `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL COMMENT '站点标题',
-  `desc` varchar(255) NOT NULL COMMENT '站点描述',
-  `keywords` varchar(255) DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '站点标题',
+  `desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '站点描述',
+  `keywords` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `state` int(11) NOT NULL COMMENT '是否开启注册',
   `verify` int(11) NOT NULL COMMENT '是否开启邮箱验证',
-  `stats` varchar(255) NOT NULL COMMENT '统计ID',
-  `smtp` varchar(255) NOT NULL COMMENT '邮箱服务器',
-  `smtp_name` varchar(255) NOT NULL COMMENT '邮箱账号',
-  `smtp_pass` varchar(255) NOT NULL COMMENT '邮箱密码',
-  `money` int(11) NOT NULL DEFAULT '10' COMMENT '用户签到金币初始值',
-  `conin` int(11) NOT NULL DEFAULT '100' COMMENT '用户注册赠送的金币',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `stats` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '统计ID',
+  `smtp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱服务器',
+  `smtp_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱账号',
+  `smtp_pass` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱密码',
+  `money` int(11) NOT NULL DEFAULT 10 COMMENT '用户签到金币初始值',
+  `conin` int(11) NOT NULL DEFAULT 100 COMMENT '用户注册赠送的金币',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of setting
 -- ----------------------------
-INSERT INTO `setting` VALUES ('1', 'MoeCTF', 'A Simple, Free CTF Platform', 'MoeCTF,CTF,WEB,CRYPTO,REVERSE,PWN,MISC,网络安全,CTF训练平台,CTFer,Hacker,黑客,信息安全', '0', '0', '', 'smtp.domain.com', '', '', '10', '100');
+INSERT INTO `setting` VALUES (1, 'MoeCTF', 'A Simple, Free CTF Platform', 'MoeCTF,CTF,WEB,CRYPTO,REVERSE,PWN,MISC,网络安全,CTF训练平台,CTFer,Hacker,黑客,信息安全', 0, 0, '', '', '', '', 10, 100);
 
 -- ----------------------------
--- Table structure for `submit`
+-- Table structure for submit
 -- ----------------------------
 DROP TABLE IF EXISTS `submit`;
-CREATE TABLE `submit` (
+CREATE TABLE `submit`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL COMMENT '用户ID',
   `cid` int(11) NOT NULL COMMENT '题目ID',
-  `value` varchar(255) NOT NULL COMMENT '提交内容',
-  `verify` int(11) NOT NULL DEFAULT '0' COMMENT '是否正确',
-  `ip` varchar(255) NOT NULL COMMENT '用户提交IP',
-  `time` varchar(255) NOT NULL COMMENT '提交时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '提交内容',
+  `verify` int(11) NOT NULL DEFAULT 0 COMMENT '是否正确',
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户提交IP',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '提交时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for `users`
+-- Table structure for submit
+-- ----------------------------
+DROP TABLE IF EXISTS `submit`;
+CREATE TABLE `submit`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL COMMENT '用户ID',
+  `cid` int(11) NOT NULL COMMENT '题目ID',
+  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '提交内容',
+  `verify` int(11) NOT NULL DEFAULT 0 COMMENT '是否正确',
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户提交IP',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '提交时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE `users`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL COMMENT '用户名',
-  `avatar` text NOT NULL COMMENT '用户头像',
-  `scores` varchar(255) DEFAULT '0' COMMENT '用户分数',
-  `password` varchar(255) NOT NULL COMMENT '用户密码',
-  `money` int(11) NOT NULL DEFAULT '0' COMMENT '用户金币',
-  `website` varchar(255) DEFAULT NULL COMMENT '用户网站',
-  `email` varchar(255) NOT NULL COMMENT '用户邮箱',
-  `content` text COMMENT '用户签名',
-  `type` varchar(255) NOT NULL DEFAULT '0' COMMENT '用户类型 0 普通用户 1 管理员',
-  `sign_days` varchar(255) DEFAULT NULL COMMENT '连续签到天数',
-  `state` int(11) NOT NULL DEFAULT '1' COMMENT '用户显示还是隐藏',
-  `verify` int(11) NOT NULL DEFAULT '0' COMMENT '用户邮箱是否验证',
-  `token` varchar(255) NOT NULL DEFAULT '' COMMENT '验证token',
-  `sign_time` varchar(255) DEFAULT NULL COMMENT '上次签到时间',
-  `mail_time` varchar(255) DEFAULT NULL COMMENT '最后一次发件时间',
-  `time` varchar(255) NOT NULL COMMENT '用户注册时间',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `avatar` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户头像',
+  `scores` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '0' COMMENT '用户分数',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户密码',
+  `money` int(11) NOT NULL DEFAULT 0 COMMENT '用户金币',
+  `website` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户网站',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户邮箱',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '用户签名',
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '用户类型 0 普通用户 1 管理员',
+  `sign_days` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '连续签到天数',
+  `state` int(11) NOT NULL DEFAULT 1 COMMENT '用户显示还是隐藏',
+  `verify` int(11) NOT NULL DEFAULT 0 COMMENT '用户邮箱是否验证',
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '验证token',
+  `sign_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '上次签到时间',
+  `mail_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '最后一次发件时间',
+  `time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户注册时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('1', 'admin', '', '', '###0918d318d77a701b5c3ad7aee135265c', '', '', '', '', '1', '2', '', '', '0', '', '1665621944');
+INSERT INTO `users` VALUES (1, 'admin', '', '', '###0918d318d77a701b5c3ad7aee135265c', 0, 'https://domain.com', 'test@domain.com', 'Power!', '1', '0', 0, 1, 'b898ff46d609f4a44a891fcc32d7c89b', '', '', '1665621944');
+
+SET FOREIGN_KEY_CHECKS = 1;
